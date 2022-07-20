02fa769a4e9e23bf9bd00a86f9841e59b223fc83
Daniel <mse19-dmicah@poly.ac.mw>
2022-07-19 21:35:33 -0700

    commiting the try1.c

---
+++ README.md
@@ -0,0 +1,3 @@
+##Try makes perfect
+<h3>Author <link> Danielmicah73@gmail.com </link> </h3>
+In this repository i'll be posting only some exaples or the try code in C language
---
+++ try1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * Recursion
+ */
+
+void print(int nb)
+{
+    if (nb < 0) 
+    {
+        return;
+    }
+    printf("%d", nb);
+    nb --;
+    print(nb);
+}
+
+int main(void)
+{
+    print(4);
+    return (0);
+}
